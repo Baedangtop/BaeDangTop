9c88add1065a898f2dff2e73459b3693f18eac69
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-07-11 17:34:07 +0900

    setting project

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import HomeStyle from "./home.style";
+
+const Home = () => {
+  return <HomeStyle></HomeStyle>;
+};
+
+export default Home;
---
+++ src/pages/Error.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ErrorStyle from "../styles/Error.style";
+
+const Error = () => {
+  return <ErrorStyle>404 Error</ErrorStyle>;
+};
+
+export default Error;
