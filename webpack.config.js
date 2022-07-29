cda4c22ec790811db11ab91ea16ab2b6cafe6a8a
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-07-29 21:48:39 +0900

    setting

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    "react-vendors": ["react", "react-dom"],
+    main: {
+      import: "./src/index.tsx",
+      runtime: "runtime",
+    },
+  },
+  output: {
+    chunkLoadTimeout: 30000,
+    filename: "[name].bundle.[contenthash].js",
+    charset: true,
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: "assets/[contenthash][ext]",
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_module/,
+        use: [
+          "babel-loader",
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, "src"), "node_modules"],
+    extensions: [".js", ".ts", ".tsx"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+};
