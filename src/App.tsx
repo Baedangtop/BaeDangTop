64cf3c8c168502253abe56ca8aef22b992d051df
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-07-30 00:50:54 +0900

    router, ssr 도입 시작

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <div>run</div>;
+  const { login } = useSelector((state: any) => state.loginReducer);
+  return (
+    <BrowserRouter>
+      {!login ? (
+        <Routes>
+          <Route path="/login" element={<div>login</div>}></Route>
+          <Route path="/register" element={<div>register</div>}></Route>
+          <Route path="*" element={<div>login</div>}></Route>
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/" element={<div>home</div>}></Route>
+          <Route path="/profile" element={<div>profile</div>}></Route>
+          <Route path="*" element={<div>404</div>}></Route>
+        </Routes>
+      )}
+    </BrowserRouter>
+  );
 };
 
 export default App;
