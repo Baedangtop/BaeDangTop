ebfcdc13a5138f1d8dd30b2b5a521f7664443132
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-08-01 18:24:59 +0900

    clean style

--- src/components/home/carousel/slider.style.ts
+++ src/components/home/carousel/slider.style.ts
@@ -8,24 +8,9 @@ export const Container = styled.div`
   background: #f7eaf2;
   border-radius: 15px;
 `;
-export const Button = styled.div`
-  all: unset;
-  padding: 1em 2em;
-  margin: 2em 2em;
-  color: burlywood;
-  border-radius: 10px;
-  border: 1px solid burlywood;
-  &:hover {
-    background-color: burlywood;
-    color: #fff;
-  }
-`;
 export const SliderContainer = styled.div`
   margin: 0 auto;
   margin-bottom: 2em;
   display: flex;
   flex-shrink: 0;
-`;
-export const Center = styled.div`
-  text-align: center;
 `;
